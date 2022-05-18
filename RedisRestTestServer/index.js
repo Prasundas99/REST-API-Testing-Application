99a1edcdbf229bdc22379a88bc1c632027158d8a
Prasundas99 <prasundas115@gmail.com>
2022-05-18 21:33:31 +0530

    feat: redis api

--- RedisRestTestServer/index.js
+++ RedisRestTestServer/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 
 import RestApifrom from './routes/Rest.js';
+import RedisApi from './routes/Redis.js';
+
 
 const app = express();
 app.use(cors());
 
 app.use('/rest', RestApifrom);
+app.use('/redis', RedisApi);
 
 app.listen(5000, () => {
   console.log("listening on port 5000");
--- RedisRestTestServer/routes/Redis.js
+++ RedisRestTestServer/routes/Redis.js
@@ -1,11 +1,27 @@
 import express from "express";
 import axios from "axios";
+import Redis from 'redis'
+
+const redisClient = Redis.createClient();
+
 
 const router = express.Router();
 
+const DEFAULT_EXPIRATION = 60 * 60 * 24; // 1 day
+
 router.get("/", async(req, res) => {
-  const {data} = await axios.get(`https://jsonplaceholder.typicode.com/photos`);
-  return res.json(data);
+  redisClient.get("photos", (err, photos) => {
+    if (err) {
+     return res.status(500).send(err);
+    }
+    if (photos != null) {
+      return res.send(JSON.parse(photos));
+    }else{
+      const {data} = await axios.get(`https://jsonplaceholder.typicode.com/photos`);
+      redisClient.setEx("photos", JSON.stringify(data), DEFAULT_EXPIRATION);
+      return res.json(data);
+    }
+  });
 });
 
 export default router;
